a1307f6891f6bc65d55c197d7ccdf8c466919d4d
Michal Koscinski <michalkoscinski15@gmail.com>
2020-10-31 23:04:12 +0000

    added localhost

--- socialMedia2/settings.py
+++ socialMedia2/settings.py
@@ -29,7 +29,7 @@ DEBUG = False
 
 
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'social-media-michal.herokuapp.com', 'testingsocial.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'social-media-michal.herokuapp.com', 'testingsocial.herokuapp.com']
 LOGIN_URL = "/login"
 
 MAX_POST_LENGTH = 240
